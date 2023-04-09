3322e89fe759a9c41bbd309d5e8910ebb1ffb7c5
Ilya Klyukovskiy <klyukovskiy@yandex.ru>
2023-04-09 16:36:05 +0300

    add actions to cell

--- uikitLessons/EmojiTableViewController.swift
+++ uikitLessons/EmojiTableViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class EmojiTableViewController: UITableViewController {
     
+    //создаем моковые данные из структуры модели
     var objects = [
         Emoji(emoji: "👨‍💻", name: "Программирование", description: "Программируй это", isFavorite: false),
         Emoji(emoji: "🎁", name: "Подарок", description: "Дарите подарки от души", isFavorite: false),
@@ -23,6 +24,8 @@ class EmojiTableViewController: UITableViewController {
         // self.clearsSelectionOnViewWillAppear = false
         
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        
+        //элменты хэдера таблицы
         self.navigationItem.leftBarButtonItem = self.editButtonItem
         self.title = "Emoji reader"
         
@@ -32,22 +35,28 @@ class EmojiTableViewController: UITableViewController {
     
     // MARK: - Table view data source
     
+    //регистрация кол-ва секций
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
     
+    //регистрация кол-ва ячеек - берем из массива данных
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return objects.count
     }
     
     
+    //регистрация ячейки из собственного класса EmojiTableViewCell, вызывается столько раз сколько передано ячеек
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        //кастим UITableViewCell к своему классу EmojiTableViewCell
         let cell = tableView.dequeueReusableCell(withIdentifier: "emojiCell", for: indexPath) as! EmojiTableViewCell
         
         let object = objects[indexPath.row]
         
+        //вынесли настройку ячейки в отдельную функцию в классе ячейки
         cell.set(object: object)
         
         
@@ -59,11 +68,12 @@ class EmojiTableViewController: UITableViewController {
         return cell
     }
     
+    //действие при нажатии на редактироние таблицы
     override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
         return .delete
     }
     
-    
+    //отслеживание нажатия на кнопку и действие с данными
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             objects.remove(at: indexPath.row)
@@ -74,12 +84,39 @@ class EmojiTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool { true }
     
     
+    //перемещение ячейки от индекса индекса
     override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        //удаляем нужну ячейку
         let moveEmoji = objects.remove(at: sourceIndexPath.row)
+        //вставляем в массив с индексом куда
         objects.insert(moveEmoji, at: destinationIndexPath.row)
+        //обновляем таблицу
         tableView.reloadData()
     }
     
+    //создание экшена по свайпу
+    override func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let like = likeAction(at: indexPath)
+        return UISwipeActionsConfiguration(actions: [like])
+    }
+    
+    //создание кнопки экена и действия с данными
+    func likeAction (at indexPath: IndexPath) -> UIContextualAction {
+        let action = UIContextualAction(style: .normal, title: "Like") { action, view, completion in
+            
+            self.objects[indexPath.row].isFavorite = !self.objects[indexPath.row].isFavorite
+            
+            completion(true)
+        }
+        action.backgroundColor = .systemBlue
+        if self.objects[indexPath.row].isFavorite {
+            action.image = UIImage(systemName: "hand.thumbsup.fill")
+        } else {
+            action.image = UIImage(systemName: "hand.thumbsup")
+        }
+        
+        return action
+    }
     
     
     
